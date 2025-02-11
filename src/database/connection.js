812388f7f13f0552e6905236e7d817716782024b
JunaidAhammedU <junaidahammed15@gmail.com>
2025-02-11 13:22:49 +0530

    Refactor database connection code for improved readability and consistency

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,22 +1,15 @@
-const mongoose = require('mongoose');
-const { DB_URL } = require('../config');
+const mongoose = require("mongoose");
+const { DB_URL } = require("../config");
 
-module.exports = async() => {
-
-    try {
-        await mongoose.connect(DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        console.log('Db Connected');
-        
-    } catch (error) {
-        console.log('Error ============')
-        console.log(error);
-        process.exit(1);
-    }
- 
+module.exports = async () => {
+  try {
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
-
- 
